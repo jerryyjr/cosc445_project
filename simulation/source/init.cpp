f1faef2268fb5e83744503474eff720418b1f117
jerryyjr <jye@colgate.edu>
2015-07-09 09:28:46 -0400

    change cond

--- simulation/source/init.cpp
+++ simulation/source/init.cpp
@@ -928,8 +928,8 @@ mutant_data* create_mutant_data (sim_data& sd, input_params& ip) {
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][1] = CW_A;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][2] = CW_A;
     mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][3] = CW_B;
-    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][4] = 20;
-    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][5] = 20;
+    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][4] = 30;
+    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][5] = 30;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][6] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][7] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][8] = CW_B;
