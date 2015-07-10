613a69bdb303222db8899a333bfb607614e2629a
jerryyjr <jye@colgate.edu>
2015-07-10 09:22:18 -0400

    refined ranges

--- simulation/source/init.cpp
+++ simulation/source/init.cpp
@@ -928,13 +928,13 @@ mutant_data* create_mutant_data (sim_data& sd, input_params& ip) {
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][1] = CW_A;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][2] = CW_A;
     mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][3] = CW_B;
-    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][4] = 30;
-    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][5] = 30;
+    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][4] = 25;
+    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][5] = 25;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][6] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][7] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][8] = CW_B;
 	mds[MUTANT_WILDTYPE].num_conditions[SEC_WAVE] = 4;
-	mds[MUTANT_WILDTYPE].cond_scores[SEC_WAVE][0] = CW_A;
+	mds[MUTANT_WILDTYPE].cond_scores[SEC_WAVE][0] = 10;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_WAVE][1] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_WAVE][2] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_WAVE][3] = CW_B;
