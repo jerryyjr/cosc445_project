fdc1a5dd7562b8e160e22ba1a7a56cb601473f9f
jerryyjr <jye@colgate.edu>
2015-07-31 17:05:57 -0400

    fix

--- simulation/source/init.cpp
+++ simulation/source/init.cpp
@@ -934,8 +934,8 @@ mutant_data* create_mutant_data (sim_data& sd, input_params& ip) {
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][1] = CW_A;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][2] = CW_B;
     mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][3] = CW_B;
-    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][4] = 10;
-    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][5] = 10;
+    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][4] = CW_A;
+    mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][5] = CW_A;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][6] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][7] = CW_B;
 	mds[MUTANT_WILDTYPE].cond_scores[SEC_ANT][8] = CW_B;
